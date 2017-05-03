8505e572f1596cf4f984997d07306f0d122aed40
dengyuchenkit <dengyuchenkit@yeah.net>
2017-05-02 21:32:59 -0400

    add dns dataset

--- src/Client.java
+++ src/Client.java
@@ -1,17 +1,86 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Random;
 
-public class client {
+public class Client {
     public static void main(String args[]) throws InterruptedException{
-        String askport = args[0];
-        int asknodeID = Integer.valueOf(args[1]);
-        String lookupName = "//localhost:" + askport + "/Listener" + asknodeID;
+        String reply;
+        int nodeNum = 0, sample = 0, count = 0;
+        HashMap<Integer, String> lookUpName = new HashMap<Integer, String>();
+        HashMap<Integer, ListenerIntf> registrationMap = new HashMap<Integer, ListenerIntf>();
+        String[] list = null;
         try {
-            ListenerIntf NodeListener = (ListenerIntf) Naming.lookup(lookupName);
-            String reply = NodeListener.clientRequest("moximoxi");
-            System.out.println("[Recieve reply] "+ reply);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        	/* read node id and registration from 'configuration' file */
+			File file = new File("configuration");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+			/* Number of nodes in the system */
+			nodeNum = Integer.valueOf(bufferedReader.readLine());
+			System.out.println(nodeNum);
+			count = 0;
+			while (count < nodeNum) {
+				line = bufferedReader.readLine();
+				list = line.split(",");
+				System.out.println(list[0] + "," + list[1]);
+				lookUpName.put(Integer.valueOf(list[0]), name(list[1], list[0]));
+				count = count + 1;
+			}
+			sample = Integer.valueOf(bufferedReader.readLine());
+			fileReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        /* add node's registration information in registrationMap */
+        lookUpName.forEach((node, uri)-> {
+        	try {
+				registrationMap.put(node, (ListenerIntf) Naming.lookup(uri));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			} catch (NotBoundException e) {
+				e.printStackTrace();
+			}
+        });
+        count = 0;
+        try {
+			File file = new File("us.csv");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+			int requestNode;
+			Random random = new Random();
+			while ((line = bufferedReader.readLine()) != null || count < sample) {
+				list = line.split(",");
+				if (list.length < 2 || list[1].length() == 0 || list[1].length() > DNSEntry.DNS_MAXLENGTH || list[0].length() > DNSEntry.IP_MAXLENGTH) {
+					continue;
+				} else {
+					count = count + 1;
+				}
+				System.out.println(list[0] + "," + list[1]);
+				requestNode = (int)(random.nextFloat() * nodeNum);
+				try {
+					reply = registrationMap.get(requestNode).clientRequest(new DNSEntry(list[0],list[1]));
+					System.out.println(reply);
+				} catch(RemoteException e) {
+					System.out.println("server " + requestNode + " loses connection");
+				}
+			}
+			fileReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }  
-
+    private static String name(String port, String node) {
+    	String lookupName = "//localhost:" + port + "/Listener" + node; 
+    	return lookupName;
+    }
 }
---
+++ src/DNSEntry.java
@@ -0,0 +1,92 @@
+import java.io.IOException;
+import java.io.Serializable;
+
+public class DNSEntry implements Serializable {
+	private static final long serialVersionUID = -2456546379723602208L;
+	public static final int DNS_MAXLENGTH = 60;
+	public static final int IP_MAXLENGTH = 32;
+	private byte[] dns;
+	private byte[] ip;
+	public DNSEntry() {
+		byte[] nullarray = new String("null").getBytes();
+		dns = new byte[DNS_MAXLENGTH];
+		System.arraycopy(nullarray, 0, dns, 0, nullarray.length);
+		ip = new byte[IP_MAXLENGTH];
+		System.arraycopy(nullarray, 0, ip, 0, nullarray.length);
+	}
+	public byte[] getDns() {
+		return dns;
+	}
+	public void setDns(byte[] dns) {
+		System.arraycopy(this.dns, 0, dns, 0, DNS_MAXLENGTH);
+	}
+	public byte[] getIp() {
+		return ip;
+	}
+	public void setIp(byte[] ip) {
+		System.arraycopy(this.ip, 0, ip, 0, IP_MAXLENGTH);
+	}
+	/**
+	 * Set dns field.
+	 */
+	public void setdns(String dns) {
+		if (dns.length() > DNS_MAXLENGTH) {
+			System.err.println("[DNSEntry setdns] string length exceeds limit.");
+		}
+		System.arraycopy(dns.toCharArray(), 0, this.dns, 0, dns.length());
+	}
+	/**
+	 * Set ip field.
+	 */
+	public void setip(String ip) {
+		if (ip.length() > IP_MAXLENGTH) {
+			System.err.println("[DNSEntry setip] string length exceeds limit.");
+		}
+		System.arraycopy(ip.toCharArray(), 0, this.ip, 0, ip.length());
+	}
+	/**
+	 * Return DNSEntry object's byte representation.
+	 */
+	public byte[] toByte() {
+		byte[] array = new byte[DNS_MAXLENGTH + IP_MAXLENGTH];
+		System.arraycopy(dns, 0, array, 0, DNS_MAXLENGTH);
+		System.arraycopy(ip, 0, array, DNS_MAXLENGTH, IP_MAXLENGTH);
+		return array;
+	}
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(new String(dns)).append(":").append(new String(ip));
+		return sb.toString();
+	}
+	/**
+	 * Create a new DNSEntry by copy the given char array.
+	 */
+	public DNSEntry(String dns, String ip) {
+		this.dns = new byte[DNS_MAXLENGTH];
+		this.ip = new byte[IP_MAXLENGTH];
+		if (dns.length() > DNS_MAXLENGTH || ip.length() > IP_MAXLENGTH) {
+			System.err.println("[DNSEntry(dns, ip)] string length exceeds limit.");
+		}
+		System.arraycopy(dns.getBytes(), 0, this.dns, 0, dns.length());
+		System.arraycopy(ip.getBytes(), 0, this.ip, 0, ip.length());
+	}
+	/**
+	 * Create DNSEntry from byte array read from a line.
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
+	 */
+	public DNSEntry(byte[] byteArray) throws IOException, ClassNotFoundException {
+		dns = new byte[DNS_MAXLENGTH];
+		ip = new byte[IP_MAXLENGTH];
+		System.arraycopy(byteArray, 0, this.dns, 0, DNS_MAXLENGTH);
+		System.arraycopy(byteArray, DNS_MAXLENGTH, this.ip, 0, IP_MAXLENGTH);
+	}
+	public boolean hasAccepted() {
+		if (new String(dns).equals("null") || new String(ip).equals("null")) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
